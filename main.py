df0eefc16e84f672c0ada92ab31a536da52712cb
Jakub Lukáš <jakublukas.eclairdesign@gmail.com>
2025-08-11 18:29:15 +0200

    Add HTML parsing script and website HTML file

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 # 45 Parsing HTML
 
+from bs4 import BeautifulSoup
+
+with open("website.html") as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents, features="html.parser")
+
+test = soup.find_all("a")
+for item in test:
+    print(item.attrs["href"])
+    
