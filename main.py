f19a9d9c97291f974ce1e05700f062b816c63ad4
Jakub Lukáš <jakublukas.eclairdesign@gmail.com>
2025-08-12 17:31:52 +0200

    Remove image extraction from movie scraping script and clean up code

--- main.py
+++ main.py
@@ -10,21 +10,14 @@ html_data = response.text
 soup = BeautifulSoup(html_data, features="html.parser")
 
 sections = soup.find_all(name="section", class_="gallery__content-item")
-
 movies = []
-images = []
 
 for section in sections:
     title = section.find(name="h3").getText()
     movies.append(title)
 
-    image = section.find(name="img", class_="landscape").get("src")
-    images.append(image)
-
-
 movies = movies[::-1]
 movies_txt = "\n".join(movies)
 
-with open("movies.txt", "w", encoding="utf-8") as file:
+with open("movies.txt", mode="w", encoding="utf-8") as file:
     file.write(movies_txt)
-
